bbd45dc1cc8d87deecb7b6a2bda094c9138ef86c
elangmfikri123 <elangmfikri123@gmail.com>
2025-05-04 13:41:14 +0700

    Update View Lampiran & Panduan

--- resources/views/adminmd/adminmd-lampiran.blade.php
+++ resources/views/adminmd/adminmd-lampiran.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.template')
-@section('title', 'Dashboard')
+@section('title', 'Lampiran & Panduan')
 @section('content')
 <div class="pcoded-content">
     <div class="pcoded-inner-content">
@@ -8,7 +8,7 @@
                 <div class="page-body">
                     <div class="card">
                         <div class="card-header">
-                            <h5>Lampiran</h5>
+                            <h5>Lampiran & Panduan</h5>
                         </div>
                         <div class="card-block table-border-style">
                             <div class="table-responsive">
@@ -23,9 +23,9 @@
                                     <tbody>
                                             <tr>
                                                 <th class="text-center" style="width: 100px;">1</th>
-                                                <td class="text-center">Lampiran Peserta</td>
+                                                <td class="text-center">Template Lampiran Peserta Customer Relation Officer</td>
                                                 <td class="text-center">
-                                                    <a href="https://drive.google.com/file/d/1VvKSFafYz0lXl2EQAD5XVQwaYgOO2jUV" 
+                                                    <a href="https://docs.google.com/spreadsheets/d/1MClKMhVJYO7VX1bEwggBJUlL06JLK92C/edit?usp=drive_link&ouid=106276902669714169102&rtpof=true&sd=true" 
                                                         target="_blank" 
                                                         class="btn btn-sm btn-info">
                                                         <i class="feather icon-download"></i> Download
@@ -34,9 +34,9 @@
                                             </tr>
                                             <tr>
                                                 <th class="text-center" style="width: 100px;">2</th>
-                                                <td class="text-center">Lampiran KLHR</td>
+                                                <td class="text-center">Template Lampiran Peserta Pimpinan Jaringan</td>
                                                 <td class="text-center">
-                                                    <a href="https://drive.google.com/file/d/1UTp1J-W_7AUzZZMXFria_AYCoANa9Gwx" 
+                                                    <a href="https://docs.google.com/spreadsheets/d/1Akxd_oixGeg6j7ZlZSxFof6saOvy-7Le/edit?usp=drive_link&ouid=106276902669714169102&rtpof=true&sd=true" 
                                                         target="_blank" 
                                                         class="btn btn-sm btn-info">
                                                         <i class="feather icon-download"></i> Download
@@ -45,6 +45,39 @@
                                             </tr>
                                             <tr>
                                                 <th class="text-center" style="width: 100px;">3</th>
+                                                <td class="text-center">Template Lampiran Peserta Team Leader</td>
+                                                <td class="text-center">
+                                                    <a href="https://docs.google.com/spreadsheets/d/1uEeXNxn4XEZH9btziejpAQPZ_viBSIA1QapulppULr8/edit?usp=drive_link" 
+                                                        target="_blank" 
+                                                        class="btn btn-sm btn-info">
+                                                        <i class="feather icon-download"></i> Download
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th class="text-center" style="width: 100px;">4</th>
+                                                <td class="text-center">Template Submission KLHR</td>
+                                                <td class="text-center">
+                                                    <a href="https://docs.google.com/spreadsheets/d/19d5IDyn23V8-Jlk0JyNVrxXzuapCZdl5/edit?usp=drive_link&ouid=106276902669714169102&rtpof=true&sd=true" 
+                                                        target="_blank" 
+                                                        class="btn btn-sm btn-info">
+                                                        <i class="feather icon-download"></i> Download
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th class="text-center" style="width: 100px;">5</th>
+                                                <td class="text-center">Template Evidence KLHR</td>
+                                                <td class="text-center">
+                                                    <a href="https://docs.google.com/presentation/d/1iviSSpHTpEkjCIHSvp7RCzktj5Fjp7kC/edit?usp=drive_link&ouid=106276902669714169102&rtpof=true&sd=true" 
+                                                        target="_blank" 
+                                                        class="btn btn-sm btn-info">
+                                                        <i class="feather icon-download"></i> Download
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th class="text-center" style="width: 100px;">6</th>
                                                 <td class="text-center">Panduan Presentasi Team Leader</td>
                                                 <td class="text-center">
                                                     <a href="https://drive.google.com/file/d/1bEE5-KU7jp_H5x3E2uMUpBX425-lcpTX/view?usp=drivesdk" 
